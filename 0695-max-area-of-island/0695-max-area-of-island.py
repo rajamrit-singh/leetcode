bda2220ea89ee1d2e64a3292919d249fe9237628
rajamrit-singh <85575979+rajamrit-singh@users.noreply.github.com>
2023-08-18 17:20:32 -0700

    Time: 130 ms (28.35%), Space: 16.1 MB (37.7%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -10,36 +10,33 @@ class Solution(object):
         """
         rows = len(grid)
         cols = len(grid[0])
-        def bfs(row, col, cur, visited):
-            # print(row)
-            # print(col)
-            # print(visited)
+        #Before looking at this bfs code, go through the code below this method
+        def bfs(row, col, visited):
+            # Adding the current row and col to the visited set
+            # Space between row and col to differentiate b/w Row - 1 and col 17("1 17") and row 11 col 7 ("11 7")
             visited.add(str(row) + " " + str(col))
             n1 = 0
             n2 = 0
             n3 = 0
             n4 = 0
             if (row + 1 < rows and grid[row + 1][col] == 1 and ((str(row + 1) + " " + str(col)) not in visited)):
-                n1 = bfs(row + 1, col, cur, visited)
+                n1 = bfs(row + 1, col,  visited)
             if (row - 1 >= 0 and grid[row - 1][col] == 1 and ((str(row - 1) + " " +  str(col)) not in visited)):
-                n2 = bfs(row - 1, col, cur, visited)
+                n2 = bfs(row - 1, col,  visited)
             if (col - 1 >= 0 and grid[row][col - 1] == 1 and ((str(row) +  " " + str(col - 1)) not in visited)):
-                n3 = bfs(row, col - 1, cur, visited)
+                n3 = bfs(row, col - 1,  visited)
             if (col + 1 < cols and grid[row][col + 1] == 1 and ((str(row) + " " +  str(col + 1)) not in visited)):
-                n4 = bfs(row, col + 1, cur, visited)
-            # print(n1)
-            # print(n2)
-            # print(n3)
-            # print(n4)
-            # print('***************')
-            return n1 + n2 + n3 + n4 + cur + 1
+                n4 = bfs(row, col + 1,  visited)
+
+            return n1 + n2 + n3 + n4  + 1
         
-        visited = set()
+        visited = set() #Create a set to keep track of all the visited elements
 
         for i in range(0, rows):
             for j in range(0, cols):
+                # If the value of a grid space is 1, then we need to run dfs on that
                 if(grid[i][j] == 1):
-                    ans = bfs(i, j, 0, visited)
+                    ans = bfs(i, j, visited)
                     if (ans > self.maxArea):
                         self.maxArea = ans
         return self.maxArea
