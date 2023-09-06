0f99213e8836ac80c23efd464e113de0fdb37ade
rajamrit-singh <85575979+rajamrit-singh@users.noreply.github.com>
2023-09-06 12:34:19 -0700

    Time: 366 ms (19.82%), Space: 37.8 MB (38.47%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -6,9 +6,13 @@ class Solution:
             res1, res2 = 0, 0
             if ((target, index) in self.memo):
                 return self.memo[(target, index)]
+            # if target is zero and we have reached the end of array then that means we
+            # found a match
             if (target == 0 and index == len(nums)):
                 return 1
-            if (target < 0 and index == len(nums)):
+            # if target is not zero and we reached the end of array then that means
+            # it is not a match
+            if (index == len(nums)):
                 return 0
             if (len(array) > 1):
                 res1 = dfs(array[1::], target - array[1], index + 1)
@@ -16,6 +20,6 @@ class Solution:
             self.memo[(target, index)] = res1 + res2
             return res1 + res2
         
-        a = dfs(nums, target - nums[0], 1)  #3
-        b = dfs(nums, target + nums[0], 1)  #2
+        a = dfs(nums, target - nums[0], 1)  
+        b = dfs(nums, target + nums[0], 1)  
         return a + b
