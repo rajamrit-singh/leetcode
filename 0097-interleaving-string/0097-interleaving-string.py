8e6847652b0089b16272aeb9a6985bf8e418ee13
rajamrit-singh <85575979+rajamrit-singh@users.noreply.github.com>
2023-09-06 20:40:05 -0700

    Time: 109 ms (5.11%), Space: 16.9 MB (46.96%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,28 +1,21 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        self.memo = {}
-        ptr = 0
-        is_s1_active = False
-        s1_ptr = 0
-        s2_ptr = 0
-        self.isPossible = False
-        def backTrack(ptr1, ptr2):
-            if ((ptr1, ptr2) in self.memo):
-                return self.memo[(ptr1, ptr2)]
-            if (ptr1 == len(s1) and ptr2 == len(s2) and ptr1 + ptr2 == len(s3)):
-                return True
-            if (ptr1 > len(s1) or ptr2 > len(s2) or ptr1 + ptr2 >= len(s3)):
-                return False
-            if(ptr1 < len(s1) and s3[ptr1 + ptr2] == s1[ptr1] and ptr2 < len(s2) and s3[ptr1 + ptr2] == s2[ptr2]):
-                res = backTrack(ptr1 + 1, ptr2) or backTrack(ptr1, ptr2 + 1)
-            elif (ptr1 < len(s1) and s3[ptr1 + ptr2] == s1[ptr1]):
-                res = backTrack(ptr1 + 1, ptr2)
-            elif (ptr2 < len(s2) and s3[ptr1 + ptr2] == s2[ptr2]):
-                res = backTrack(ptr1, ptr2 + 1)
-            else:
-                return False
-            self.memo[(ptr1, ptr2)] = res
-            return res
-        a = backTrack(0, 0)
-        return a
-            
+        if (len(s1) + len(s2) != len(s3)):
+            return False
+        dp = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
+        dp[len(s1)][len(s2)] = True
+        print(dp)
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                print(i)
+                print(j)
+                print('****************')
+                if(i < len(s1) and s3[i + j] == s1[i] and dp[i + 1][j]):
+                    dp[i][j] = True
+                # if (i == 1 and j == 0):
+                #     print(s3[i+j])
+                #     print(s2[j])
+                #     print(dp[i][j+1])
+                if (j < len(s2) and s3[i + j] == s2[j] and dp[i][j+1]):
+                    dp[i][j] = True
+        return dp[0][0]
