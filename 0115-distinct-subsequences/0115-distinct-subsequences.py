94dbfeabc28d2bbfc57791faca982c4dfc3de3da
rajamrit-singh <85575979+rajamrit-singh@users.noreply.github.com>
2023-09-07 10:44:28 -0700

    Time: 854 ms (14.34%), Space: 159.6 MB (13.56%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -4,15 +4,18 @@ class Solution:
         self.memo = {}
         def dfs(current, index):
             length = len(current)
-            if (current != t[:length]):
-                return 0
+            # if (current != t[:length]):
+            #     return 0
             if((current, index) in self.memo):
                 return self.memo.get((current, index))
             if (current == t):
                 return 1
             if (len(current) == len(t) or index >= len(s)):
                 return 0
-            res1 = dfs(current + s[index], index + 1)
+            if (s[index] == t[length]):                
+                res1 = dfs(current + s[index], index + 1)
+            else:
+                res1 = 0
             res2 = dfs(current, index + 1)
             self.memo[(current, index)] = res1 + res2
             return res1 + res2
