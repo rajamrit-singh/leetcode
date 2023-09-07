27e8a64c583870b90713733e2c1f259af84baaa0
rajamrit-singh <85575979+rajamrit-singh@users.noreply.github.com>
2023-09-07 10:41:24 -0700

    Time: 1055 ms (5.03%), Space: 159.6 MB (13.56%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        
+        self.memo = {}
+        def dfs(current, index):
+            length = len(current)
+            if (current != t[:length]):
+                return 0
+            if((current, index) in self.memo):
+                return self.memo.get((current, index))
+            if (current == t):
+                return 1
+            if (len(current) == len(t) or index >= len(s)):
+                return 0
+            res1 = dfs(current + s[index], index + 1)
+            res2 = dfs(current, index + 1)
+            self.memo[(current, index)] = res1 + res2
+            return res1 + res2
+        
+        result = dfs("", 0)
+        return result
