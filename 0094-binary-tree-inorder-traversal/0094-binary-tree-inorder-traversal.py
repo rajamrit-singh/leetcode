d1b0052287ca75065cb51a0860b082802efc13e0
rajamrit-singh <85575979+rajamrit-singh@users.noreply.github.com>
2024-05-19 17:28:14 -0700

    Time: 38 ms (34.68%), Space: 16.5 MB (24.91%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -10,21 +10,13 @@ class Solution:
             return root
         stack = []
         result = []
-        # stack.append(root)
         node = root
-        stack.append(node)
-        node = node.left
         while (len(stack) > 0 or node != None):
-            if (node == None):
-                node = stack.pop()
-                result.append(node.val)
-                node = node.right
-                continue
-            if (node.left):
-                stack.append(node)
-                node = node.left
-            else:
-                result.append(node.val)
-                node = node.right
-                
+                if (node):
+                    stack.append(node)
+                    node = node.left
+                else:
+                    node = stack.pop()
+                    result.append(node.val)
+                    node = node.right
         return result
